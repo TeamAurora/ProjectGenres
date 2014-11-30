0de2d7b578c399919a4117c170d090a058bebfdd
Craig Jeffrey <Craig-J@users.noreply.github.com>
2014-11-30 00:53:37 +0000

    Revert "Imported Code"
    This reverts commit b665d3a09f0b4c303d24938ba5d10a2a9bf123f2.

--- game_settings.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct GameSettings
-{
-	enum DIFFICULTY { EASY, MEDIUM, HARD, EXTREME, TRANSCENDENT}; // possible difficulties
-	DIFFICULTY difficulty_;
-	bool music_; // toggles music on and off
-	bool sound_effects_; // toggles sound effects on and off
-};
